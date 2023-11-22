ce4d51d191c628774909bc9798f4514c77ff317f
Félix Larose-Gervais <filedesless@gmail.com>
2023-11-22 14:18:19 -0500

    caching is hard and unnecessary

--- INF7541/random_automatic/main.py
+++ INF7541/random_automatic/main.py
@@ -1,10 +1,12 @@
 import random
 import functools
 import string
+import graphviz
 
 K = 2
 STATE_NUMBER = random.randint(5, 10)
-OUTPUT_LEN = 10
+OUTPUT_LEN = 100
+
 
 def main():
     dfa = DFA(K, STATE_NUMBER)
@@ -15,6 +17,7 @@ def main():
         print(f'{i} -> {term} ({string.ascii_letters[term]})')
         output += string.ascii_letters[term]
     print(output)
+    dfa.view()
 
 
 class DFA:
@@ -23,18 +26,15 @@ class DFA:
         self.input_alphabet = range(k)
         self.initial_state = random.choice(self.states)
         self.output_alphabet = range(n)
-        print(
-            f'random automata with {n} states and alphabet of size {k} starting on {self.initial_state}')
-
-    @functools.cache
-    def transition(self, state: int, symbol: int) -> int:
-        assert state in self.states
-        assert symbol in self.input_alphabet
-        return random.choice(self.states)
+        self.transition = {(state, symbol): random.choice(self.states)
+                           for state in self.states for symbol in self.input_alphabet}
+        self.output = {state: random.choice(self.output_alphabet) for state in self.states}
+        self.desc = f'random automata with {n} states and alphabet of size {k} starting on {self.initial_state}'
+        print(self.desc)
 
     def extended_transition(self, state: int, symbols: list[int]) -> int:
         for symbol in symbols:
-            state = self.transition(state, symbol)
+            state = self.transition[(state, symbol)]
         return state
 
     def read_int(self, number: int) -> int:
@@ -46,15 +46,21 @@ class DFA:
         digits.reverse()
         state = self.extended_transition(self.initial_state, digits)
         print(f'executing automaton on string {digits} leads to state {state}')
-        output = self.output(state)
-        print(f'which maps to output', output)
+        output = self.output[state]
+        print('which maps to output', output)
         return output
 
-    @functools.cache
-    def output(self, state: int) -> int:
-        assert state in self.states
-        return random.choice(self.output_alphabet)
+    def view(self):
+        dot = graphviz.Digraph()
+        for state in self.states:
+            dot.node(str(state), f"{state} | {self.output[state]}")
+            for symbol in self.input_alphabet:
+                dst = self.transition[(state, symbol)]
+                dot.edge(str(state), str(dst), str(symbol))
+        dot.node('start')
+        dot.edge('start', str(self.initial_state))
+        dot.view()
+
 
 if __name__ == '__main__':
     main()
-
